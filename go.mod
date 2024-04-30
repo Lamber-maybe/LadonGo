ee6143f1d9137e4c4f9b14ac0b1b7fedac023149
Lamber <43318547+Lamber-maybe@users.noreply.github.com>
2024-04-30 09:41:25 +0000

    Analysis PingScan

--- go.mod
+++ go.mod
@@ -5,9 +5,11 @@ go 1.13
 require (
 	github.com/alouca/gologger v0.0.0-20120904114645-7d4b7291de9c // indirect
 	github.com/alouca/gosnmp v0.0.0-20170620005048-04d83944c9ab
+	github.com/armon/go-socks5 v0.0.0-20160902184237-e75332964ef5 // indirect
 	github.com/denisenkom/go-mssqldb v0.9.0
 	github.com/fatih/color v1.10.0
 	github.com/go-ldap/ldap v3.0.3+incompatible
+	github.com/go-routeros/routeros v0.0.0-20210123142807-2a44d57c6730 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/godror/godror v0.23.1
 	github.com/hirochachacha/go-smb2 v1.0.3
@@ -16,5 +18,6 @@ require (
 	github.com/stacktitan/smb v0.0.0-20190531122847-da9a425dceb8
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 	golang.org/x/text v0.3.5
+	golang.org/x/time v0.5.0 // indirect
 	gopkg.in/asn1-ber.v1 v1.0.0-20181015200546-f715ec2f112d // indirect
 )
--- ping/ping.go
+++ ping/ping.go
@@ -1,108 +1,116 @@
 ﻿package ping
-//Ladon Scanner for golang 
+
+//Ladon Scanner for golang
 //Author: k8gege
 //K8Blog: http://k8gege.org/Ladon
 //Github: https://github.com/k8gege/LadonGo
 import (
 	"os/exec"
 	//"log"
-	"fmt"
 	"bytes"
+	"fmt"
 	"runtime"
 	"strings"
+
 	"github.com/k8gege/LadonGo/str"
 )
-//Support User
-func CmdPing(host string) ( result bool,err error)  {
-    sysType := runtime.GOOS
-    if sysType == "linux" {
-		cmd := exec.Command("/bin/sh","-c","ping -c 1 "+host)
+
+// Support User
+func CmdPing(host string) (result bool, err error) {
+	sysType := runtime.GOOS
+
+	// @Author Lamber
+	// 在linux系统中，运行 "/bin/sh -c ping -c 1 host" 进行探测
+	// 在windows系统中，运行 "cmd /c ping -a -n 1 host" 进行探测
+
+	if sysType == "linux" {
+		cmd := exec.Command("/bin/sh", "-c", "ping -c 1 "+host)
 		var out bytes.Buffer
 		cmd.Stdout = &out
 		cmd.Run()
 		//err := cmd.Run()
 		//if err != nil {
-			//log.Fatal(err)
+		//log.Fatal(err)
 		//}
 		//fmt.Println(out.String())
 		if strings.Contains(out.String(), "ttl=") {
 			//fmt.Println("ISOK")
 			result = true
-		 } 
-    } else if sysType == "windows" {
-		cmd := exec.Command("cmd","/c","ping -a -n 1 "+host)
+		}
+	} else if sysType == "windows" {
+		cmd := exec.Command("cmd", "/c", "ping -a -n 1 "+host)
 		var out bytes.Buffer
 		cmd.Stdout = &out
 		cmd.Run()
 		//err := cmd.Run()
 		//if err != nil {
-			//log.Fatal(err)
+		//log.Fatal(err)
 		//}
 		//fmt.Println(out.String())
 		if strings.Contains(out.String(), "TTL=") {
 			//fmt.Println("ISOK")
 			result = true
-		 } 
-    }
-    return result,err
+		}
+	}
+	return result, err
 }
 
-func PingOK(host string) ( result bool)  {
-    sysType := runtime.GOOS
-    if sysType == "linux" {
-		cmd := exec.Command("/bin/sh","-c","ping -c 1 "+host)
+func PingOK(host string) (result bool) {
+	sysType := runtime.GOOS
+	if sysType == "linux" {
+		cmd := exec.Command("/bin/sh", "-c", "ping -c 1 "+host)
 		var out bytes.Buffer
 		cmd.Stdout = &out
 		cmd.Run()
 		if strings.Contains(out.String(), "ttl=") {
 			//fmt.Println("ISOK")
 			result = true
-		 } 
-    } else if sysType == "windows" {
-		cmd := exec.Command("cmd","/c","ping -n 1 "+host)
+		}
+	} else if sysType == "windows" {
+		cmd := exec.Command("cmd", "/c", "ping -n 1 "+host)
 		var out bytes.Buffer
 		cmd.Stdout = &out
 		cmd.Run()
 		if strings.Contains(out.String(), "TTL=") {
 			//fmt.Println("ISOK")
 			result = true
-		 } 
-    }
-    return result
+		}
+	}
+	return result
 }
 
-func PingName(host string) ( result bool,err error)  {
-    sysType := runtime.GOOS
-    if sysType == "linux" {
-		cmd := exec.Command("/bin/sh","-c","ping -c 1 "+host)
+func PingName(host string) (result bool, err error) {
+	sysType := runtime.GOOS
+	if sysType == "linux" {
+		cmd := exec.Command("/bin/sh", "-c", "ping -c 1 "+host)
 		var out bytes.Buffer
 		cmd.Stdout = &out
 		cmd.Run()
 		//err := cmd.Run()
 		//if err != nil {
-			//log.Fatal(err)
+		//log.Fatal(err)
 		//}
 		//fmt.Println(out.String())
 		if strings.Contains(out.String(), "ttl=") {
-			fmt.Println("PING: "+host)
+			fmt.Println("PING: " + host)
 			result = true
-		 } 
-    } else if sysType == "windows" {
-		cmd := exec.Command("cmd","/c","ping -a -n 1 "+host)
+		}
+	} else if sysType == "windows" {
+		cmd := exec.Command("cmd", "/c", "ping -a -n 1 "+host)
 		var out bytes.Buffer
 		cmd.Stdout = &out
 		cmd.Run()
 		//err := cmd.Run()
 		//if err != nil {
-			//log.Fatal(err)
+		//log.Fatal(err)
 		//}
 		//fmt.Println(out.String())
 		if strings.Contains(out.String(), "TTL=") && strings.Contains(out.String(), "["+host+"]") {
-			fmt.Println("PING: "+host+"\t"+str.GetBetween(out.String(),"Pinging", " ["+host+"]"))
+			fmt.Println("PING: " + host + "\t" + str.GetBetween(out.String(), "Pinging", " ["+host+"]"))
 			result = true
-		 } else if strings.Contains(out.String(), "TTL="){
-			fmt.Println("PING: "+host)
-		 }		 
-    }
-    return result,err
+		} else if strings.Contains(out.String(), "TTL=") {
+			fmt.Println("PING: " + host)
+		}
+	}
+	return result, err
 }
